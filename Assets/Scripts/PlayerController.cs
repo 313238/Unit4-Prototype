e50420c7d8f7342849f456a1657eb6e8a1dd5bf2
Abdel2102 <313238@student.mboutrecht.nl>
2021-05-21 18:36:43 +0200

    Added scripts and made the game function

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private GameObject focalPoint;
+    private AudioSource aS;
+    private float powerupStrength = 15.0f;
+    public float speed = 5.0f;
+    public bool hasPowerup = false;
+    public GameObject powerupIndicator;
+    
+
+
+
+    // Start is called before the first frame update    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        aS = GetComponent<AudioSource>();
+        focalPoint = GameObject.Find("Focal Point");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+
+        playerRb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerupIndicator.gameObject.SetActive(true);
+            aS.Play();
+            Debug.Log("CHIDORIII" + aS.clip);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+
+            Rigidbody enemeyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+            enemeyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with powerup set to " + hasPowerup);
+           
+        }
+    }
+}
+
+/*bounce.Play();
+Debug.Log("CHIDORIII" + bounce.clip);
+bounce = GetComponent<AudioSource>();
+private AudioSource bounce;*/
+
+
